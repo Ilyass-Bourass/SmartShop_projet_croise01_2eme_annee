3e321df26679a3ed67b19af40717fd3cf3e96792
ilyass bourass <ilyass.b.1992@gmail.com>
2025-11-26 18:45:20 +0100

    feat:créer le mapper pour une commande

---
+++ src/main/java/com/smartshop/smartshop/mapper/commande/CommandeMapper.java
@@ -0,0 +1,26 @@
+package com.smartshop.smartshop.mapper.commande;
+
+
+import com.smartshop.smartshop.dto.commande.RequestCommandeDTO;
+import com.smartshop.smartshop.dto.commande.ResponseCommandeDTO;
+import com.smartshop.smartshop.entity.Commande;
+import com.smartshop.smartshop.mapper.ligneCommande.LigneCommandeMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring" ,uses = {LigneCommandeMapper.class})
+public interface CommandeMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target ="numeroCommande" , expression = "java(creerNumeroCommande())")
+    Commande toCommande(RequestCommandeDTO requestCommandeDTO);
+
+    @Mapping(target = "idClient", source = "client.id")
+    @Mapping(target = "nomClient", source = "client.nom")
+    ResponseCommandeDTO toResponseCommandeDTO(Commande commande);
+
+
+    default String creerNumeroCommande(){
+        String numeroCommande = "CMD-"+ java.util.UUID.randomUUID().toString().substring(0,7);
+        return numeroCommande;
+    }
+}
